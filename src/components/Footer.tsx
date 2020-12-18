40f6a4ba9b447ef7ec99e104168ef63245d94d90
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-12-18 16:35:23 +0000

    Add styling for header and footer bg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,12 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   return (
     <div id={styles.Footer}>
-      <p>Github LinkedIn</p>
-      <a href="mailto:replaceThisWith-first.last@gmail.com">
-        first.last@gmail.com
-      </a>
+      <div id={styles.contactIcons}>Github LinkedIn</div>
+      <div id={styles.contactEmail}>
+        <a href="mailto:replaceThisWith-first.last@gmail.com">
+          first.last@gmail.com
+        </a>
+      </div>
     </div>
   );
 };
